66d0e6f849f5a44797106998d3686433964bae51
Simó Albert i Beltran <sim6@probeta.net>
2015-09-04 11:33:25 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bankbarcode',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/gisce/bankbarcode',
+    license='GNU Affero General Public License v3',
+    author='Simó Albert i Beltran',
+    author_email='sim6@probeta.net',
+    description='barcodes for financial documents'
+)
