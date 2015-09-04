88fee48575de1217095161098a815a7bcfcf04f3
Simó Albert i Beltran <sim6@probeta.net>
2015-09-04 13:03:07 +0200

    spec of checksum generation for Recibo507 of cuaderno57

---
+++ spec/cuaderno57_Recibo507_checksum_spec.py
@@ -0,0 +1,30 @@
+from expects import expect, equal
+from bankbarcode.cuaderno57 import Recibo507
+
+with description('Checksum Recibo507 of cuaderno57'):
+    with it('accomplish the example of cuaderno57.pdf'):
+        entity = '01234567'
+        suffix = '023'
+        ref = '12345678901'
+        id = '123456'
+        amount = '6543.21'
+        checksum = '74'
+        expect(Recibo507(entity, suffix, ref, id, amount).checksum()).to(equal(checksum))
+
+    with it('accomplish another example'):
+        entity = '22350466'
+        suffix = '501'
+        ref = '00000000015'
+        id = '300815'
+        amount = '53.98'
+        checksum = '68'
+        expect(Recibo507(entity, suffix, ref, id, amount).checksum()).to(equal(checksum))
+
+    with it('accomplish another example'):
+        entity = '22350466'
+        suffix = '501'
+        ref = '00000000401'
+        id = '300815'
+        amount = '37.62'
+        checksum = '56'
+        expect(Recibo507(entity, suffix, ref, id, amount).checksum()).to(equal(checksum))
