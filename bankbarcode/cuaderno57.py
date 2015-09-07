7cf80581d83c2aca89f8a4204c8f6255c7295f7d
Simó Albert i Beltran <sim6@probeta.net>
2015-09-07 13:10:42 +0200

    use property and setter decorations

--- bankbarcode/cuaderno57.py
+++ bankbarcode/cuaderno57.py
@@ -44,21 +44,56 @@ class Recibo(BankBarcode):
 class Recibo507(Recibo):
 
     def __init__(self, entity, suffix, ref, notice, amount):
+        self.entity = entity
+        self.suffix = suffix
+        self.ref = ref
+        self.notice = notice
+        self.amount = amount
 
-        if self._check_entity(entity):
-            self.entity = entity
+    @property
+    def entity(self):
+        return self._entity.zfill(8)
 
-        if self._check_suffix(suffix):
-            self.suffix = suffix
+    @entity.setter
+    def entity(self, value):
+        if self._check_entity(value):
+            self._entity = value
 
-        if self._check_ref(ref):
-            self.ref = ref
+    @property
+    def suffix(self):
+        return self._suffix.zfill(3)
 
-        if self._check_notice(notice):
-            self.notice = notice
+    @suffix.setter
+    def suffix(self, value):
+        if self._check_suffix(value):
+            self._suffix = value
 
-        if self._check_amount(amount):
-            self.amount = amount
+    @property
+    def ref(self):
+        return self._ref.zfill(11)
+
+    @ref.setter
+    def ref(self, value):
+        if self._check_ref(value):
+            self._ref = value
+
+    @property
+    def notice(self):
+        return self._notice.zfill(6)
+
+    @notice.setter
+    def notice(self, value):
+        if self._check_notice(value):
+            self._notice = value
+
+    @property
+    def amount(self):
+        return self._amount.zfill(10)
+
+    @amount.setter
+    def amount(self, value):
+        if self._check_amount(value):
+            self._amount = value
 
     def checksum(self):
         amount100 = int(Decimal(self.amount) * 100)
