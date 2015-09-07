fa3703648fc881baa8e0ecc162e42892f9515538
Simó Albert i Beltran <sim6@probeta.net>
2015-09-07 17:07:04 +0200

    spec for BankBarcode

---
+++ spec/BankBarcode_spec.py
@@ -0,0 +1,20 @@
+from expects import expect, raise_error
+from bankbarcode.bankbarcode import BankBarcode
+
+with description('BankBarcode'):
+    with it('doesn\'t have code generation method'):
+        error = 'This method is not implemented!'
+
+        def callback():
+            return BankBarcode().code()
+
+        expect(callback).to(raise_error(NotImplementedError, error))
+
+    with it('can\'t create barcode without code generation method'):
+        path = '/tmp/mybarcode'
+        error = 'This method is not implemented!'
+
+        def callback():
+            return BankBarcode().save(path)
+
+        expect(callback).to(raise_error(NotImplementedError, error))
