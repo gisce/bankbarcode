e2daba18e222baf9339a894eb739509600b73622
Simó Albert i Beltran <sim6@probeta.net>
2015-09-07 11:33:24 +0200

    use Semantic Versioning

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bankbarcode',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     url='https://github.com/gisce/bankbarcode',
     license='GNU Affero General Public License v3',
