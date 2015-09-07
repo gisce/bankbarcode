e782b547b2bcc808451c02b340f45bf767967ae8
Simó Albert i Beltran <sim6@probeta.net>
2015-09-07 17:36:27 +0200

    document BankBarcode

--- bankbarcode/bankbarcode.py
+++ bankbarcode/bankbarcode.py
@@ -2,8 +2,20 @@ from barcode import generate
 
 
 class BankBarcode(object):
+    """
+    Base class for barcodes
+    """
 
     def _check_length(self, name, value, expected_length, description):
+        """
+        Check length of a value.
+
+        :param name: name of the value
+        :param value: the value itself
+        :param expected_length: the expected lenght of the value
+        :param description: definition of the expected value
+        :return: True if value have the expected lenght, otherwise False
+        """
         length = len(value)
         if length < expected_length:
             raise ValueError('{} is too short, {}'.format(name, description))
@@ -13,9 +25,17 @@ class BankBarcode(object):
             return True
 
     def code(self):
+        """
+        Code generation. To implement in child classes
+        """
         raise NotImplementedError('This method is not implemented!')
 
     def save(self, path):
+        """
+        Save barcode in SVG format.
+
+        :param path: path to SVG file without ".svg" extension
+        """
         writer_options = {'font_size': 6}
         generate(
             'code128',
