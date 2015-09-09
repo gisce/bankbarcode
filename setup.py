89b8bae98cfebb48d5cade6eede0d1606f0de79c
Simó Albert i Beltran <sim6@probeta.net>
2015-09-09 17:08:48 +0200

    dependency_links of pyBarcode to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ setup(
     license='GNU Affero General Public License v3',
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
+    # We need python-barcode v0.8, to have Code128 (EAN128), not released yet
+    # https://bitbucket.org/whitie/python-barcode/issues/16/pypi-08-release-request
+    dependency_links=[
+        "https://bitbucket.org/whitie/python-barcode/get/6c22b96a2ca2.zip"
+    ],
     install_requires=[
         'pybarcode>=0.8b1'
     ],
